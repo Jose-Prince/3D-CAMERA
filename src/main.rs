90f74ee4085074d454711be08cf3982dd0c11046
Jose-Prince <joseprince148@gmail.com>
2024-08-28 17:21:50 -0600

    Feat: Can draw a bear

--- src/main.rs
+++ src/main.rs
@@ -24,38 +24,73 @@ fn main() {
 
     let fur = Material::new(color::Color::new(255,255,255));
     let skin = Material::new(color::Color::new(250,175,188));
+    let mouth_m = Material::new(color::Color::new(240,240,240));
+    let black = Material::new(color::Color::new(0,0,0));
     
     let right_ear: Sphere = Sphere {
-        center: Vec3::new(1.7,2.0,-5.0),
+        center: Vec3::new(1.7,2.0,-7.0),
         radius: 1.2,
         material: fur,
     };
 
     let left_ear: Sphere = Sphere {
-        center: Vec3::new(-1.7,2.0,-5.0),
+        center: Vec3::new(-1.7,2.0,-7.0),
         radius: 1.2,
         material: fur,
     };
 
     let head: Sphere = Sphere {
-        center: Vec3::new(0.0,0.0,-10.0),
-        radius: 5.0,
+        center: Vec3::new(0.0,0.0,-5.0),
+        radius: 2.0,
         material: fur,
     };
 
     let inside_right_ear: Sphere = Sphere {
-        center: Vec3::new(0.0,0.0,-4.0),
-        radius: 1.0,
+        center: Vec3::new(1.5,1.8,-6.0),
+        radius: 0.6,
         material: skin,
     };
 
     let inside_left_ear: Sphere = Sphere {
-        center: Vec3::new(1.7,2.0,-5.0),
-        radius: 1.2,
-        material: fur,
+        center: Vec3::new(-1.5,1.8,-6.0),
+        radius: 0.6,
+        material: skin,
     };
 
-    let objects = vec![head,right_ear, left_ear, inside_right_ear];
+    let mouth: Sphere = Sphere {
+        center: Vec3::new(0.0,-0.55,-3.5),
+        radius: 0.9,
+        material: mouth_m,
+    };
+
+    let nose: Sphere = Sphere {
+        center: Vec3::new(0.0,-0.2,-2.0),
+        radius: 0.2,
+        material: black,
+    };
+
+    let eye_r: Sphere = Sphere {
+        center: Vec3::new(0.2,0.3,-2.0),
+        radius: 0.15,
+        material: black,
+    };
+
+    let eye_l: Sphere = Sphere {
+        center: Vec3::new(-0.2,0.3,-2.0),
+        radius: 0.15,
+        material: black,
+    };
+
+    let objects = vec![
+        head,right_ear, 
+        left_ear, 
+        inside_right_ear, 
+        inside_left_ear,
+        mouth,
+        nose,
+        eye_r,
+        eye_l,
+    ];
 
     render(&mut framebuffer, &objects);
 
